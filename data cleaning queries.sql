862a226aa758a3e2a1de664361fffeb8fa7d0399
THARUN1024 <badavaththarun000@gmail.com>
2025-01-14 16:12:09 +0530

    Update data cleaning queries.sql

--- data cleaning queries.sql
+++ data cleaning queries.sql
@@ -30,7 +30,7 @@ WHERE company = 'Casper';
 -- CREATING NEW TABLE 
 
 
-CREATE TABLE layoffs_stagging2 (
+CREATE TABLE layoffs_staging2 (
 company text,
 location text,
  industry text,
@@ -45,10 +45,10 @@ location text,
 
 
 SELECT *
-FROM layoffs_stagging2;
+FROM layoffs_staging2;
 
 -- INSERTING DATA INTO  TABLE--> layoffs_stagging2
-INSERT INTO layoffs_stagging2
+INSERT INTO layoffs_staging2
 SELECT *,
 ROW_NUMBER() OVER( PARTITION BY company, location, industry, total_laid_off,
  percentage_laid_off, `date`, stage, country, funds_raised_millions ) AS  row_num
@@ -58,19 +58,19 @@ FROM layoffs_staging;
 --  CHECKING IF THERE IS ANY DUPLICATE
 
 SELECT *
-FROM layoffs_stagging2
+FROM layoffs_staging2
 WHERE row_num > 1;
 
 -- DELETING DUPLICATE 
 
 DELETE 
-FROM layoffs_stagging2
+FROM layoffs_staging2
 WHERE row_num > 1;
 
 --  VIEWING THE TABLE
 
 SELECT *
-FROM layoffs_stagging2;
+FROM layoffs_staging2;
 
 
 -- STANDARDIZING  THE DATA--
@@ -78,39 +78,39 @@ SELECT company, TRIM(company)
 FROM layoffs_stagging2;
 
 
-UPDATE layoffs_stagging2
+UPDATE layoffs_staging2
 SET company = TRIM(company);
 
 SELECT DISTINCT industry
-FROM layoffs_stagging2
+FROM layoffs_staging2
 ORDER BY 1;
 
-UPDATE layoffs_stagging2
+UPDATE layoffs_staging2
 SET industry = 'Crypto'
 WHERE industry LIKE 'Crypto%';
 
 SELECT DISTINCT country
-FROM layoffs_stagging2
+FROM layoffs_staging2
 ORDER BY 1;
 
-UPDATE layoffs_stagging2
+UPDATE layoffs_staging2
 set country = TRIM(TRAILING '.' FROM country)
 WHERE country LIKE 'United States%';
 
 -- Editing date type to date column--
 
-UPDATE layoffs_stagging2
+UPDATE layoffs_staging2
 SET `date` = str_to_date(`date`, '%m/%d/%Y'); -- a way to convert date type into date column by string--
 
 SELECT `date`
-FROM layoffs_stagging2;
+FROM layoffs_staging2;
 
-ALTER TABLE layoffs_stagging2
+ALTER TABLE layoffs_staging2
 MODIFY COLUMN `date` DATE;   -- NOW it is converted into date 
 
 SELECT t1.industry, t2.industry
-FROM layoffs_stagging2 t1
-JOIN layoffs_stagging2 t2
+FROM layoffs_staging2 t1
+JOIN layoffs_staging2 t2
 ON T1.company = t2.company
 WHERE (t1.industry IS NULL OR t1.industry = '')
 AND t2.industry IS NOT NULL;
@@ -118,12 +118,12 @@ AND t2.industry IS NOT NULL;
 
 
 
-UPDATE layoffs_stagging2
+UPDATE layoffs_staging2
 SET industry = NULL
 WHERE industry = '';
 
-UPDATE layoffs_stagging2  t1
-JOIN layoffs_stagging2 t2
+UPDATE layoffs_staging2  t1
+JOIN layoffs_staging2 t2
 	ON t1.company = t2.company
 SET t1.industry = t2.industry
 WHERE t1.industry IS NULL
@@ -131,7 +131,7 @@ AND t2.industry IS NOT NULL;
 
 -- Checking the values which are null in both total_laid_off and percentage_laid_off 
 SELECT *
-FROM layoffs_stagging2
+FROM layoffs_staging2
 WHERE total_laid_off IS NULL
 AND percentage_laid_off IS NULL
 ;
@@ -140,16 +140,16 @@ AND percentage_laid_off IS NULL
 -- Deleting the values which are null in both total_laid_off and percentage_laid_off 
 
 DELETE 
-FROM layoffs_stagging2
+FROM layoffs_staging2
 WHERE total_laid_off IS NULL
 AND percentage_laid_off IS NULL
 ;
 
 SELECT *
-FROM layoffs_stagging2
+FROM layoffs_staging2
 ;
 -- Deleting row_num column
-ALTER TABLE layoffs_stagging2
+ALTER TABLE layoffs_staging2
 DROP COLUMN row_num
 ;
 
@@ -165,65 +165,65 @@ DROP COLUMN row_num
 -- EXPLORATORY DATA ANALYSIS
 
 SELECT country, SUM(total_laid_off) AS total_off, COUNT(total_laid_off)
-FROM layoffs_stagging2
+FROM layoffs_staging2
 GROUP BY country
 ;
 
 SELECT  COUNT(country)
-FROM layoffs_stagging2
+FROM layoffs_staging2
 WHERE country = 'United States'
 AND total_laid_off IS NULL
 ;
 
 SELECT  COUNT(country)
-FROM layoffs_stagging2
+FROM layoffs_staging2
 WHERE country = 'United States'
 ;
 
 
 -- SELECT  `date`, SUM(total_laid_off) AS total_off, COUNT(total_laid_off)
 SELECT  YEAR(`date`), SUM(total_laid_off) AS total_off
-FROM layoffs_stagging2
+FROM layoffs_staging2
 GROUP BY YEAR(date)
 ORDER BY 2 DESC
 ;
 
 SELECT *
-FROM layoffs_stagging2;
+FROM layoffs_staging2;
 
 
 SELECT company, SUM(total_laid_off)
-FROM layoffs_stagging2
+FROM layoffs_staging2
 GROUP BY company
 ORDER BY 2 DESC
 ;
 
 SELECT industry, SUM(total_laid_off)
-FROM layoffs_stagging2
+FROM layoffs_staging2
 GROUP BY industry        -- it is nescessarry to put group by when we use aggregate function eg SUM, MAX, ETC
 ORDER BY 2 DESC
 ;
 
 SELECT country, SUM(total_laid_off)
-FROM layoffs_stagging2
+FROM layoffs_staging2
 GROUP BY country
 ORDER BY 2 DESC
 ;
 
 SELECT  stage, SUM(total_laid_off) AS total_off
-FROM layoffs_stagging2
+FROM layoffs_staging2
 GROUP BY stage
 ORDER BY 2 DESC
 ;
 
 SELECT company, SUM(total_laid_off)
-FROM layoffs_stagging2
+FROM layoffs_staging2
 WHERE stage = 'Post-IPO'
 GROUP BY company 
 ORDER BY 2 DESC;
 
 SELECT SUBSTRING(`date`,1,7) AS `MONTH`, SUM(total_laid_off)
-FROM layoffs_stagging2
+FROM layoffs_staging2
 WHERE SUBSTRING(`date`,1,7) IS NOT NULL
 GROUP BY `MONTH`
 ORDER BY 1 ;
@@ -232,7 +232,7 @@ ORDER BY 1 ;
 WITH Rolling_total AS 
 (
 SELECT SUBSTRING(`date`,1,7) AS `MONTH`, SUM(total_laid_off) AS total_off
-FROM layoffs_stagging2
+FROM layoffs_staging2
 WHERE SUBSTRING(`date`,1,7) IS NOT NULL
 GROUP BY `MONTH`
 ORDER BY 1 
@@ -241,14 +241,14 @@ SELECT `MONTH`, SUM(total_off) OVER (ORDER BY `MONTH`) AS rolling_total
 FROM Rolling_total;
 
 SELECT company, YEAR(`date`), SUM(total_laid_off)
-FROM layoffs_stagging2
+FROM layoffs_staging2
 GROUP BY company,  YEAR(`date`)
 ORDER BY 3 DESC;
 
 WITH company_year (company, years, total_laid_off) AS
 (
 SELECT company, YEAR(`date`), SUM(total_laid_off)
-FROM layoffs_stagging2
+FROM layoffs_staging2
 GROUP BY company,  YEAR(`date`)
 ), company_year_rank AS 
 (
